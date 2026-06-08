7227240e4feb3bd34be1e3e8f949e41834f8bc1e
syafiqsny-debug <syafiqsny@gmail.com>
2026-06-08 10:26:45 +0800

    Update project.py

--- project.py
+++ project.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-st.title("""Welcome to my Dashboard
-This is my first time using streamlit.""")
+st.image('bmwlogo.png')
+st.title("""Welcome to my Dashboard""")
 
 DF = pd.read_csv('bmw (1).csv')
 st.subheader("Raw Data")
